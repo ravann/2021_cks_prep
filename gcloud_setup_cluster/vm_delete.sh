80fe2b664c6838038a1b9929d68cbf372591f73a
Ravan Nannapaneni <ravan_n@hotmail.com>
2021-08-21 16:11:51 +0000

    Added worker to delete statement ...

--- gcloud_setup_cluster/vm_delete.sh
+++ gcloud_setup_cluster/vm_delete.sh
@@ -1,1 +1,3 @@
 gcloud compute instances delete k8master --delete-disks=all --zone=asia-southeast1-b --quiet
+
+gcloud compute instances delete k8worker --delete-disks=all --zone=asia-southeast1-b --quiet
